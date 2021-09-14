1b26658bd1e91c51f2a246cb7e19501ae8fc74d6
mrabets <newrabgo@gmail.com>
2021-09-14 17:44:24 +0300

    Replace form with erb

--- app/controllers/rock_games_controller.rb
+++ app/controllers/rock_games_controller.rb
@@ -1,8 +1,11 @@
 class RockGamesController < ApplicationController
   def new
+    @rock_game = RockGame.new
+    @types = ['rock paper scissors', 'rock spock paper lizard scissors']
   end
 
   def create
+    redirect_to root_path
   end
 
   def show
