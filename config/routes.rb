aaa0b679e25ca12a0a53321e1da43ef354dc1acb
mrabets <newrabgo@gmail.com>
2021-09-14 17:26:50 +0300

    Add rock_game routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'rock_games/new'
-  get 'rock_games/create'
-  get 'rock_games/show'
-  get 'rock_games/index'
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     sessions: 'users/sessions'
@@ -17,5 +13,8 @@ Rails.application.routes.draw do
       patch 'batch_operation'
     end
   end
+
+  resources :rock_games
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
