c1888b29ca14344fc128cb58b5bffc522d597fcc
mrabets <newrabgo@gmail.com>
2021-09-16 11:08:19 +0300

    Add id to room channel

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,17 +1,23 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-    console.log("Connected")
-  },
+document.addEventListener('turbolinks:load', () => {
+  const element = document.getElementById("room-id")
+  const room_id = element.getAttribute('data-room-id')
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+  consumer.subscriptions.create({channel: "RoomChannel", room_id: room_id},  {
+    connected() {
+      console.log("Connected with room id " + room_id)
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    console.log(data)
-  }
-});
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      $(`#msg-${room_id}`).append('<div class="message"> ' + data.content + '</div>')
+
+      console.log(data.content)
+    }
+  });
+})
