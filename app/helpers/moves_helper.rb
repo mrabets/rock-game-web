a74cef236d01b29ecd60ac14c29d968983c1b422
mrabets <newrabgo@gmail.com>
2021-09-19 16:09:08 +0300

    Add help table and change game algorithm

--- app/helpers/moves_helper.rb
+++ app/helpers/moves_helper.rb
@@ -4,9 +4,18 @@ module MovesHelper
 
       if (row_moves.map(&:content).size == 2)
         players_id = row_moves.map(&:user_id)
+
         moves = row_moves.map(&:content)
+
         first_player_name, second_player_name = User.find(players_id[0]).name, User.find(players_id[1]).name   
-        result_message = get_message_result(moves[0], moves[1], first_player_name, second_player_name)       
+        
+        result_message = get_message_result(
+          moves[0],
+          moves[1],
+          first_player_name,
+          second_player_name,
+          room_id)       
+        
         SendMoveJob.perform_later(move, result_message)
         Move.destroy_by(room_id: room_id)
       else
@@ -16,21 +25,36 @@ module MovesHelper
 
   private
 
-  def get_message_result(first_move, second_move, first_player_name, second_player_name)
+  def get_message_result(first_move, second_move, first_player_name, second_player_name, room_id)
       number_result = ->a,b{(2+a.sum%88%6-b.sum%88%6)%5%3}
       number = number_result.call(first_move.to_s, second_move.to_s)
 
-      case number
-      when 0
-        return "#{first_player_name} win"
-      when 1
-        return "#{second_player_name} two win"
-      end
+      first_index = Room.find(room_id).name.split.find_index(first_move)
+      second_index = Room.find(room_id).name.split.find_index(second_move)
 
-      "Draw"
+      n = Room.find(room_id).name.split.size
+      number = (n + first_index - second_index) % n
+
+      if number == 0
+        return "Draw"
+      elsif number % 2 == 0
+        return "#{second_player_name} win"
+      elsif number % 2 == 1
+        return "#{first_player_name} win"
+      end
     end
 
     def get_two_players_ids
       Move.where(room_id: move_params[:room_id]).limit(2).map(&:user_id)
+    end
+
+    def get_message(number)
+      if number == 0
+        return "DRAW"
+      elsif number % 2 == 1
+        return "LOSE"
+      elsif number % 2 == 0
+        return "WIN"
+      end
     end
 end
